e09c49d918c11fb01780d7e9cc2eccda8cc63300
Hossein <hossein.bajan@gmail.com>
2024-06-19 11:34:11 +0330

    Install bootstrap and config it, add layout

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,20 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  modules: [],
+  devtools: { enabled: true },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "~/assets/scss/_colors.scss" as *;',
+        },
+      },
+    },
+  },
+  css: ["~/assets/scss/style.scss"],
+  plugins: ["~/plugins/bootstrap.client"],
+  postcss: {
+    plugins: {
+      rtlcss: {},
+    },
+  },
+});
