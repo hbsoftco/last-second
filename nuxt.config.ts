b1efd230840745f4ec99837096c3bd09c6867703
Hossein <hossein.bajan@gmail.com>
2024-06-19 11:36:02 +0330

    Add rtl configuration using postcss  RTLCSS

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,4 +17,26 @@ export default defineNuxtConfig({
       rtlcss: {},
     },
   },
+  app: {
+    pageTransition: { name: "page", mode: "out-in" },
+    layoutTransition: { name: "layout", mode: "out-in" },
+    head: {
+      charset: "utf-8",
+      viewport:
+        "width=device-width, initial-scale=1, maximum-scale=5, user-scalable=yes",
+      htmlAttrs: {
+        lang: "fa",
+        dir: "rtl",
+      },
+      title: "hbsoft test app",
+      meta: [
+        { name: "theme-color", content: "#000000" },
+        {
+          hid: "description",
+          name: "description",
+          content: "Your description here",
+        },
+      ],
+    },
+  },
 });
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <NuxtLayout>
-
+            hossein
         </NuxtLayout>
     </div>
 </template>
